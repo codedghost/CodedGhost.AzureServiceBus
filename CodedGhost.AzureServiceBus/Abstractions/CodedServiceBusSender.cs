6cde98ea34a3505aa4994a428d3cb59c477489e9
codedghost2 <codedghost2@gmail.com>
2022-08-26 16:24:38 +0100

    Adding basic implementation

---
+++ CodedGhost.AzureServiceBus/Abstractions/CodedServiceBusSender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Azure.Messaging.ServiceBus.Administration;
+using CodedGhost.AzureServiceBus.Helpers;
+using CoreCodedChatbot.Secrets;
+using Newtonsoft.Json;
+
+namespace CodedGhost.AzureServiceBus.Abstractions
+{
+    public class CodedServiceBusSender<T> : ServiceBusSender
+    {
+        public CodedServiceBusSender(ISecretService secretService)
+        : base(ServiceBusHelper.GetClient<T>(secretService), ServiceBusHelper.GetQueueName<T>())
+        {
+        }
+
+        public Task SendMessageAsync(T message, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var serviceBusMessage = new ServiceBusMessage(JsonConvert.SerializeObject(message));
+
+            return base.SendMessageAsync(serviceBusMessage, cancellationToken);
+        }
+    }
+
+    public class CodedServiceBusReceiver<T> : ServiceBusReceiver
+    {
+        public CodedServiceBusReceiver(ISecretService secretService, ServiceBusReceiveMode receiveMode = ServiceBusReceiveMode.PeekLock)
+            : base(ServiceBusHelper.GetClient<T>(secretService), ServiceBusHelper.GetQueueName<T>(),
+                new ServiceBusReceiverOptions {ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete})
+        {
+        }
+    }
+}
---
+++ CodedGhost.AzureServiceBus/Helpers/ServiceBusHelper.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Messaging.ServiceBus;
+using CoreCodedChatbot.Secrets;
+using System.Threading.Tasks;
+using System;
+using Azure.Messaging.ServiceBus.Administration;
+
+namespace CodedGhost.AzureServiceBus.Helpers
+{
+    public static class ServiceBusHelper
+    {
+        public static ServiceBusClient GetClient<T>(ISecretService secretService)
+        {
+            var queueName = GetQueueName<T>();
+
+            var asyncClient = new Lazy<Task<ServiceBusClient>>(async () =>
+            {
+                var connectionString = secretService.GetSecret<string>("ServiceBusConnectionString");
+                var adminClient =
+                    new ServiceBusAdministrationClient(connectionString);
+
+                var exists = await adminClient.QueueExistsAsync(queueName);
+
+                if (!exists.Value)
+                {
+                    await adminClient.CreateQueueAsync(queueName);
+                }
+
+                return new ServiceBusClient($"{connectionString}");
+            });
+
+            return asyncClient.Value.Result;
+        }
+
+        public static string GetQueueName<T>() => $"{typeof(T).Name}Queue";
+    }
+}
